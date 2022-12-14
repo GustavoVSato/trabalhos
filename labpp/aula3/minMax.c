00c2212ca9864d187589beb7de36ad5c7399e1e1
GustavoVSato <115162022+GustavoVSato@users.noreply.github.com>
2022-12-14 20:06:20 -0300

    Add files via upload

---
+++ labpp/aula3/minMax.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int num[n];
+    int i;
+    for(i=0;i<n;i++){
+        int v;
+        scanf("%d",&v);
+        num[i]=v;
+    }
+    int Me=num[0];
+    int Ma=0;
+    int a;
+    for(a=0;a<n;a++){
+        if(Ma<=num[a]){
+            Ma=num[a];
+        }
+    }
+    int b;
+    for(b=0;b<n;b++){
+        if(Me>=num[b]){
+            Me=num[b];
+        }
+    }
+    int k;
+    for(k=0;k<n;k++){
+        printf("%d",num[k]);
+        if(num[k]==Me){
+            printf("%s","<");
+        }
+        if(num[k]==Ma){
+            printf("%s",">");
+        }
+        printf("%s"," ");
+    }
+    
+    
+
+    return 0;
+}
